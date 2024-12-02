80b6cadada0b6cd42f1dfed34ea3b1204185bcf9
lcoba <lcoba@outlook.com>
2024-12-01 20:35:49 -0500

    Nuevos selects

--- TallerDeBasesDeDatos/ConsultasSELECT.sql
+++ TallerDeBasesDeDatos/ConsultasSELECT.sql
@@ -10,3 +10,33 @@ SELECT BusinessEntityID, FirstName, MiddleName, LastName
 	FROM Person.Person
 		WHERE BusinessEntityID = 262
 
+
+-- SELECT para mostrar los campos/columnas de la tabla Person.Person
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Person'
+
+-- JOINS de las tablas Person.Person, Person.Address y 
+-- Person.BussinessEntityAddress 
+SELECT * 
+	FROM Person.Person PP 
+	INNER JOIN Person.BusinessEntityAddress PB ON PP.BusinessEntityID = PB.BusinessEntityID
+	INNER JOIN Person.Address PA ON PB.AddressID = PA.AddressID
+		WHERE PP.BusinessEntityID = 262
+
+
+-- Mismo SELECT que el anterior pero solamente mostrando los datos 
+-- BussinessEntityID, FirsName, LastName, AddressLine1, City
+SELECT PP.BusinessEntityID, PP.FirstName, PP.LastName, PA.AddressLine1, PA.City
+	FROM Person.Person PP 
+	INNER JOIN Person.BusinessEntityAddress PB ON PP.BusinessEntityID = PB.BusinessEntityID
+	INNER JOIN Person.Address PA ON PB.AddressID = PA.AddressID
+		WHERE PP.BusinessEntityID = 262
+
+
+
+
+
+
+
+
+
+
