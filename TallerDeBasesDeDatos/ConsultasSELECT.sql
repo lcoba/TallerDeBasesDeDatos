94cf6552c6001ea175085b4312c927d8afd89ac5
lcoba <lcoba@outlook.com>
2024-12-01 20:26:11 -0500

    Nuevos selects

--- TallerDeBasesDeDatos/ConsultasSELECT.sql
+++ TallerDeBasesDeDatos/ConsultasSELECT.sql
@@ -4,3 +4,9 @@ USE AdventureWorks2022
 -- SELECT para mostrar todos los campos de todos los registros de la tabla Person.Person
 SELECT * FROM Person.Person
 
+-- SELECT para mostrar los campos BusinessEntityID, FirstName, MiddleName y LastName
+-- de la tabla Person.Person donde el BusinessEntityName es igual a 22
+SELECT BusinessEntityID, FirstName, MiddleName, LastName 
+	FROM Person.Person
+		WHERE BusinessEntityID = 262
+
