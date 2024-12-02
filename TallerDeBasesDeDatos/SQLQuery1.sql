28032232acb28e442c33c6976d4ff1d4db6751ea
lcoba <lcoba@outlook.com>
2024-12-01 20:10:00 -0500

    Primer Commit

---
+++ TallerDeBasesDeDatos/SQLQuery1.sql
@@ -0,0 +1,6 @@
+-- Comando para usar la base de datos AdventureWorks2022
+USE AdventureWorks2022
+
+-- SELECT para mostrar todos los campos de todos los registros de la tabla Person.Person
+SELECT * FROM Person.Person
+
